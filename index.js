614a47cf07401ca9752e6e6da2831e0d11a88aa4
nagasaireddy <115387069+nagasaidev12@users.noreply.github.com>
2022-12-18 11:42:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const a=document.getElementById("p1")
+a.innerHTML="hey"
+console.log(a)
+const b=document.getElementById("p2")
+b.innerHTML="my "
+console.log(b)
+const c=document.getElementById("p3")
+c.innerHTML="name"
+console.log(c)
+const u=document.getElementById("p4")
+u.innerHTML="is"
+console.log(u)
+const d=document.getElementById("p5")
+var p= prompt("enter your name here")
+d.innerHTML= p
+console.log(p)
+const e=document.getElementById("p6")
+e.innerHTML="and"
+console.log(e)
+const f=document.getElementById("p7")
+f.innerHTML="my"
+console.log(f)
+const g=document.getElementById("p8")
+g.innerHTML="age"
+console.log(g)
+const h=document.getElementById("p9")
+h.innerHTML="is"
+console.log(h)
+const i=document.getElementById("p10")
+var k=prompt("enter your age here ")
+i.innerHTML= k
+console.log(k)
+const j=document.getElementById("p11")
+j.innerHTML="i"
+console.log(j)
+const l=document.getElementById("p12")
+l.innerHTML="love"
+console.log(l)
+const m=document.getElementById("p13")
+var o=prompt("enter name of movie you love")
+m.innerHTML=o
+console.log(o)
